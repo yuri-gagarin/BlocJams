ef0b2554b8c5c67e816e8fe79f0662c77ebc54ae
Yuriy Ivanov <ymivanov@icloud.com>
2016-08-15 18:02:37 -0400

    checkpoint-9-DOM/Scripting/Events Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,8 @@
-var animatePoints = function () {
- 
-    var points = document.getElementsByClassName('point');
-    
+var pointsArray = document.getElementsByClassName("point");
+
+
+var animatePoints = function (points) {
+     
     var revealPoint = function (index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
@@ -13,4 +14,18 @@ var animatePoints = function () {
     };
 };
 
-animatePoints();
+ window.onload = function() {
+     
+     if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+     
+     var sellingPoints = document.getElementsByClassName("selling-points")[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);
+        }
+     });
+ }
